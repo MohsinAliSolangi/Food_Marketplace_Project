65e3988108cde8e929468d1d5ab58fda6fa941db
Muddassirali110 <muddassirali110786@gmail.com>
2024-12-15 21:47:49 +0500

    change pinata upload

--- Frontend/src/pages/Uploadpage.js
+++ Frontend/src/pages/Uploadpage.js
@@ -13,62 +13,64 @@ function Uploadpage() {
   const navigate = useNavigate();
 
   //TODO::uncommit this for production
-  // const handleSubmit = async (product) => {
-  //   console.log(product, "productproductproduct");
-
-  //   if (typeof product?.image !== "undefined") {
-  //     let resut
-  //     try {
-  //       resut = await uploadFileToIPFS(product?.image);
-  //     } catch (error) {
-  //       console.log("ipfs image fails",error);
-  //     }
-
-  //     const nftJSON = {
-  //       attributes: [
-  //         { trait_type: `Variety`, value: `${product?.variety}` },
-  //         { trait_type: "Origin", value: `${product?.origin}` },
-  //         { trait_type: "Weight", value: `${product?.weight}` },
-  //       ],
-  //       description: `${product?.description}`,
-  //       image: `${resut?.pinataURL}`,
-  //       name: `${product?.name}`,
-  //     };
-
-  //     try {
-  //       const result = await uploadJSONToIPFS(nftJSON);
-  //       console.log("this is json image format ", result);
-
-  //       if (!product?.currentPrice || !product?.biddingDeadline || !result) return;
-
-  //       await mintThenList(product?.currentPrice,product?.biddingDeadline,result);
-  //       navigate("/marketplace");
-  //     } catch (error) {
-  //       console.log("ipfs uri upload error: ", error);
-  //     }
-  //   }
-  // };
-
   const handleSubmit = async (product) => {
     console.log(product, "productproductproduct");
-    try {
-      const timestamp = convertToUnixTimestamp(product?.biddingDeadline);
 
-      console.log(
-        timestamp,
-        "product?.biddingDeadlineproduct?.biddingDeadline"
-      );
+    if (typeof product?.image !== "undefined") {
+      let resut
+      try {
+        resut = await uploadFileToIPFS(product?.image);
+      } catch (error) {
+        console.log("ipfs image fails",error);
+      }
 
-      await mintThenList(
-        product?.currentPrice,
-        timestamp,
-        "https://ipfs.io/ipfs/QmeLfHUuJS1DYSvXmXP2vfZRCFhLF1wDUvUXFbPWQYMCna"
-      );
-      navigate("/marketplace");
-    } catch (error) {
-      console.log("ipfs uri upload error: ", error);
+      const nftJSON = {
+        attributes: [
+          { trait_type: `Variety`, value: `${product?.variety}` },
+          { trait_type: "Origin", value: `${product?.origin}` },
+          { trait_type: "Weight", value: `${product?.weight}` },
+        ],
+        description: `${product?.description}`,
+        image: `${resut?.pinataURL}`,
+        name: `${product?.name}`,
+      };
+
+      try {
+        const result = await uploadJSONToIPFS(nftJSON);
+        console.log("this is json image format ", result);
+
+        if (!product?.currentPrice || !product?.biddingDeadline || !result) return;
+        
+        const timestamp = convertToUnixTimestamp(product?.biddingDeadline);
+
+        await mintThenList(product?.currentPrice,timestamp,result);
+        navigate("/marketplace");
+      } catch (error) {
+        console.log("ipfs uri upload error: ", error);
+      }
     }
   };
+
+  // const handleSubmit = async (product) => {
+  //   console.log(product, "productproductproduct");
+  //   try {
+  //     const timestamp = convertToUnixTimestamp(product?.biddingDeadline);
+
+  //     console.log(
+  //       timestamp,
+  //       "product?.biddingDeadlineproduct?.biddingDeadline"
+  //     );
+
+  //     await mintThenList(
+  //       product?.currentPrice,
+  //       timestamp,
+  //       "https://ipfs.io/ipfs/QmeLfHUuJS1DYSvXmXP2vfZRCFhLF1wDUvUXFbPWQYMCna"
+  //     );
+  //     navigate("/marketplace");
+  //   } catch (error) {
+  //     console.log("ipfs uri upload error: ", error);
+  //   }
+  // };
 
   function convertToUnixTimestamp(dateString) {
     const date = new Date(dateString);
